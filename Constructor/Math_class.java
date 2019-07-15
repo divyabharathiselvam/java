1dfdb9cd7b74dd16d4a5c4f4f66e5ff2c429556d
divyabharathiselvam <51196088+divyabharathiselvam@users.noreply.github.com>
2019-07-15 19:00:06 +0530

    Add files via upload

---
+++ Constructor/Math_class.java
@@ -0,0 +1,14 @@
+package Langpackages;
+import static java.lang.System.*;
+import static java.lang.Math.*;
+public class Math_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=(int)ceil(21.1);
+System.out.println(x);
+int y=(int)floor(21.1);
+System.out.println(y);
+	}
+
+}
