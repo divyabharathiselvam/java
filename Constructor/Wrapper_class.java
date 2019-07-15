a2097d99ae2ccaf1f4ed30f971c816b18e9685ad
divyabharathiselvam <51196088+divyabharathiselvam@users.noreply.github.com>
2019-07-15 18:57:24 +0530

    Add files via upload

---
+++ Constructor/Wrapper_class.java
@@ -0,0 +1,14 @@
+package Langpackages;
+
+public class Wrapper_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(Byte.MIN_VALUE+"to"+Byte.MAX_VALUE);
+System.out.println(Integer.MIN_VALUE+"to"+Integer.MAX_VALUE);
+System.out.println(Long.MIN_VALUE+"to"+Long.MAX_VALUE);
+Integer i=new Integer(10);
+System.out.println(i+100);
+	}
+
+}
