b4168c225ce1e7d9c272956491f71f33b4654f8c
divyabharathiselvam <51196088+divyabharathiselvam@users.noreply.github.com>
2019-07-16 18:34:39 +0530

    Add files via upload

---
+++ Collection_interface/array_class.java
@@ -0,0 +1,18 @@
+package Collection_interface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+public class array_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int arr[]={12,23,4,5,6,77};
+   Arrays.sort(arr);
+   int arrr[]=arr;
+    for(int a:arrr)
+System.out.println(a);
+  
+	}
+}
